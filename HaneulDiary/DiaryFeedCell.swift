5a077dc9a99ed3762dde73eb849868e7dbbb5a72
ehdudgns <justin941@naver.com>
2021-11-20 00:46:51 +0900

    피드 페이지 구현중

---
+++ HaneulDiary/DiaryFeedCell.swift
@@ -0,0 +1,43 @@
+//
+//  DiaryFeedCell.swift
+//  HaneulDiary
+//
+//  Created by 도영훈 on 2021/11/19.
+//
+
+import UIKit
+import SDWebImage
+
+class DiaryFeedCell: UITableViewCell {
+    @IBOutlet weak var skyImage: UIImageView!
+    @IBOutlet weak var diaryContext: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setStyle()
+        skyImage.layer.cornerRadius = 10
+        
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+    
+    func setStyle(){
+        skyImage.layer.masksToBounds = false
+        skyImage.layer.shadowRadius = 10
+        skyImage.layer.shadowOffset = .zero
+        skyImage.layer.shadowOpacity = 0.5
+        skyImage.layer.shadowColor = UIColor.black.cgColor
+    }
+    
+    func configure(image : URL?,context : String){
+        skyImage.sd_setImage(with: image, completed: nil)
+        diaryContext.text = context
+    }
+    
+}
--- HaneulDiary/FeedViewController.swift
+++ HaneulDiary/FeedViewController.swift
@@ -10,10 +10,17 @@ import UIKit
 class FeedViewController: UIViewController {
     
    
-    
+    @IBOutlet weak var feedtableView: UITableView!
+    let images : [String] = ["https://mblogthumb-phinf.pstatic.net/MjAxNzAyMDJfMjg5/MDAxNDg1OTk2ODc2Njc0.YLls08aCtZPAMR8GBJ4ktIadCtUzpPeS1jdzBTkt6nUg.W-p_93_51zJ9YvoxTevbSB0WO3YV1F0Qr91XpepRrg0g.JPEG.pho6400/1.jpg?type=w800"]
+    let contexts : [String] = ["와 날씨 좋다"]
     override func viewDidLoad() {
         super.viewDidLoad()
         print("here feed!!!")
+        feedtableView.dataSource = self
+        feedtableView.delegate = self
+        feedtableView.register(UINib(nibName: "DiaryFeedCell", bundle: nil), forCellReuseIdentifier: "DiaryFeedCell")
+
+        
         // Do any additional setup after loading the view.
     }
     
@@ -28,4 +35,22 @@ class FeedViewController: UIViewController {
     }
     */
 
+}
+extension FeedViewController:UITableViewDelegate{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "DiaryFeedCell", for: indexPath) as? DiaryFeedCell else{
+            return DiaryFeedCell()
+        }
+        cell.configure(image: URL(string: images[indexPath.row]), context: contexts[indexPath.row])
+        return cell
+    }
+}
+extension FeedViewController:UITableViewDataSource{
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 500
+    }
 }

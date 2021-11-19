d5724fa3ca81504d29d5641aa1e5c475da87e643
ehdudgns <justin941@naver.com>
2021-11-20 08:39:58 +0900

    엥? 해결됨 ㅋㅋ왜 해결됬지??

--- HaneulDiary/CalendarViewController.swift
+++ HaneulDiary/CalendarViewController.swift
@@ -15,12 +15,9 @@ class CalendarViewController: UIViewController {
     let images : [String] = ["https://images.pexels.com/photos/907485/pexels-photo-907485.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260","https://mblogthumb-phinf.pstatic.net/MjAxNzAyMDJfMjg5/MDAxNDg1OTk2ODc2Njc0.YLls08aCtZPAMR8GBJ4ktIadCtUzpPeS1jdzBTkt6nUg.W-p_93_51zJ9YvoxTevbSB0WO3YV1F0Qr91XpepRrg0g.JPEG.pho6400/1.jpg?type=w800"]
     
     override func viewDidLoad() {
-        print("ddddd")
         super.viewDidLoad()
-        print("here cal!!!!")
         self.calendarCollectionView.dataSource = self
         self.calendarCollectionView.delegate = self
-        self.calendarCollectionView.register(CalendarCell.self, forCellWithReuseIdentifier: "CalendarCell")
     }
 }
 
@@ -57,8 +54,6 @@ class CalendarCell :UICollectionViewCell{
     }
 
     func configure(image : URL?){
-        print(image)
-        print(skyImage)
         self.skyImage.sd_setImage(with: image, completed: nil)
     }
 }

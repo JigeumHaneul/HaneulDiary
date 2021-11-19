ef53b45f3058d836b01cf87cd5fb381e920d7e83
ehdudgns <justin941@naver.com>
2021-11-20 03:14:32 +0900

    피드 추가 화면 구현

---
+++ HaneulDiary/AddFeed/AddFeedViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AddFeedViewController.swift
+//  HaneulDiary
+//
+//  Created by 도영훈 on 2021/11/20.
+//
+
+import UIKit
+
+class AddFeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    @IBAction func backBtn(_ sender: Any) {
+        self.dismiss(animated: false, completion: nil)
+    }
+}
--- HaneulDiary/FeedViewController.swift
+++ HaneulDiary/FeedViewController.swift
@@ -10,9 +10,11 @@ import UIKit
 class FeedViewController: UIViewController {
     
    
+   
     @IBOutlet weak var feedtableView: UITableView!
     let images : [String] = ["https://mblogthumb-phinf.pstatic.net/MjAxNzAyMDJfMjg5/MDAxNDg1OTk2ODc2Njc0.YLls08aCtZPAMR8GBJ4ktIadCtUzpPeS1jdzBTkt6nUg.W-p_93_51zJ9YvoxTevbSB0WO3YV1F0Qr91XpepRrg0g.JPEG.pho6400/1.jpg?type=w800"]
     let contexts : [String] = ["와 날씨 좋다"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print("here feed!!!")
@@ -21,21 +23,18 @@ class FeedViewController: UIViewController {
         feedtableView.register(UINib(nibName: "DiaryFeedCell", bundle: nil), forCellReuseIdentifier: "DiaryFeedCell")
 
         
-        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func addFeedContext(_ sender: Any) {
+        let vc = UIStoryboard(name: "AddFeedView", bundle: nil).instantiateViewController(withIdentifier: "AddFeedView")
+        vc.modalPresentationStyle = .overFullScreen
+        self.present(vc, animated: false, completion: nil)
     }
     
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
+
 extension FeedViewController:UITableViewDelegate{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 1
@@ -49,6 +48,7 @@ extension FeedViewController:UITableViewDelegate{
         return cell
     }
 }
+
 extension FeedViewController:UITableViewDataSource{
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 500

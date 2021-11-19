13b76e36682a7bfab451a562c7004fd3297661ab
ehdudgns <justin941@naver.com>
2021-11-20 04:52:45 +0900

    피드 추가 구현중

--- HaneulDiary/AddFeed/AddFeedViewController.swift
+++ HaneulDiary/AddFeed/AddFeedViewController.swift
@@ -6,12 +6,39 @@
 //
 
 import UIKit
+import Combine
+import Photos
+import PhotosUI
 
-class AddFeedViewController: UIViewController {
-
+class AddFeedViewController: UIViewController,PHPhotoLibraryChangeObserver {
+    
+    var fetchResult = PHFetchResult<PHAsset>()
+    
+    var canAccessImages: [UIImage] = []
+    var thumbnailSize: CGSize {
+        let scale = UIScreen.main.scale
+        return CGSize(width: (UIScreen.main.bounds.width / 3) * scale, height: 100 * scale)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    func requestPHPhotoLibraryAuthorization(completion: @escaping () -> Void) {
+        PHPhotoLibrary.requestAuthorization(for: .readWrite) { (status) in
+            switch status {
+            case .limited:
+                PHPhotoLibrary.shared().register(self)
+                completion()
+            default:
+                break
+            }
+        }
+    }
+    
+    func photoLibraryDidChange(_ changeInstance: PHChange) {
+        print("oh chage???")
+    }
+    
     @IBAction func backBtn(_ sender: Any) {
         self.dismiss(animated: false, completion: nil)
     }

eab70a8a74444fb8d3e73f2971859859080843b1
yeniful <dev.yeeun@gmail.com>
2021-11-20 14:32:32 +0900

    Splash 추가. viewController 연결 필요

---
+++ HaneulDiary/SplashViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  HaneulDiary
+//
+//  Created by Yeni Hwang on 2021/11/20.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        <#code#>
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
